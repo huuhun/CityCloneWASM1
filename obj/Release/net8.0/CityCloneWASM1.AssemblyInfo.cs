f1b034bf85a03e42da53e9005068d3df21a3930a
Le Tran Huu Hung <huuhung@namiproj.com>
2024-08-13 13:44:03 +0700

    added some js

--- obj/Release/net8.0/CityCloneWASM1.AssemblyInfo.cs
+++ obj/Release/net8.0/CityCloneWASM1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CityCloneWASM1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+61f67f4075918dd7e6db7635e66024b883b86a14")]
 [assembly: System.Reflection.AssemblyProductAttribute("CityCloneWASM1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CityCloneWASM1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
