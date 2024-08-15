18bb694fc5d515484dfe68b71fafd16a7fca6f8e
Le Tran Huu Hung <huuhung@namiproj.com>
2024-08-15 17:34:30 +0700

    fixed the 2nd main layout of iframes

--- obj/Debug/net8.0/CityCloneWASM1.AssemblyInfo.cs
+++ obj/Debug/net8.0/CityCloneWASM1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CityCloneWASM1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d1a6792b3844c09679eb0489f14374bdebb059c5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6d1488fc5d92dc536eb2c517501c9e9811951fc4")]
 [assembly: System.Reflection.AssemblyProductAttribute("CityCloneWASM1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CityCloneWASM1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
