9b05ba8fc264374c89e274c98f1ceb9dd227960f
Le Tran Huu Hung <huuhung@namiproj.com>
2024-08-16 10:12:13 +0700

    added effect to dropup

--- obj/Debug/net8.0/CityCloneWASM1.AssemblyInfo.cs
+++ obj/Debug/net8.0/CityCloneWASM1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CityCloneWASM1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+18bb694fc5d515484dfe68b71fafd16a7fca6f8e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4630c6ad6f669335551d87cd93c7b210b2db6378")]
 [assembly: System.Reflection.AssemblyProductAttribute("CityCloneWASM1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CityCloneWASM1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
